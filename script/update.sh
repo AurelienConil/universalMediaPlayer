6eb8ee610b842bcd97f02b59b77c8ce377e9f83f
undessens <assoundessens@gmail.com>
2020-06-26 14:45:06 +0200

    update script and .gitignore Makefile

--- script/update.sh
+++ script/update.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-cd /home/pi/of/apps/universalMediaPlayer
+cd /home/pi/Documents/openFrameworks/apps/universalMediaPlayer
 echo "GIT PULL"
 git pull
 #better call git reset --hard HEAD
-cd node/public
-echo "RUN BUILD OF WEBAPP"
-npm run build
-cd ../../
 echo "BUILD OF APP"
 cd of_universalMediaPlayer
-make -j2
+sudo make -j2
 echo "UPDATE IS DONE"
 
