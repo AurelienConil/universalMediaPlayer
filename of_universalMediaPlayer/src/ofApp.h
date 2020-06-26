90893cd96b42a3994e287a496c9f5912d631a05c
AurelienConil <conil.a@hotmail.fr>
2020-06-26 19:25:31 +0200

    change port according to figma documentation

--- of_universalMediaPlayer/src/ofApp.h
+++ of_universalMediaPlayer/src/ofApp.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "PlatformConfig.h"
 #define OSC_PORT_RECEIVE 12343
-#define OSC_PORT_SEND 12345
+#define OSC_PORT_SEND 12344
 
 #include "ofMain.h"
 #include "videoPlayer.hpp"
